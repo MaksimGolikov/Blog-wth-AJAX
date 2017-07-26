3e40e25dffe7f04ff15b410d80fc974f24dbb777
Golikov Maks <golikov.mo@gmail.com>
2017-07-26 10:55:19 +0300

    Add files via upload

---
+++ ViewModels/ChatViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Blog.Models.Chatt;
+using Blog.Models;
+
+namespace Blog.ViewModels
+{
+    public class ChatViewModel
+    {
+        public List<Chat> MyChats {get;set;}
+        public List<Chat> ICreated {get;set;}
+        public IEnumerable<User> AllUsers {get;set;}
+        public ForMasterPage MasterPage { get; set; }
+    }
+}
---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Blog.Models;
+
+namespace Blog.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public ForMasterPage MasterPage { get; set; }
+    }
+}
