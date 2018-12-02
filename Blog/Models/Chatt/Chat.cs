df3541b26dd68c693d71ba8b929367561c2b49c4
MaksimGolikov <golikov.mo@gmail.com>
2018-12-02 10:56:45 +0200

    Chat file has been added

---
+++ Blog/Models/Chatt/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models.Chatt
+{
+    public class Chat
+    {
+        public int ID {get;set;}
+        public string ChatName { get; set; }
+        public int CreatorID { get; set; }
+        public string  Users { get; set; }
+        
+    }
+}
